6059de02c7fbda4a831ad6ef6f4d1482f3af40a5
Tasbeha Nawaz <tasbehanawaz@gmail.com>
2023-12-21 18:04:33 +0000

    Modified .json files

---
+++ src/LoginForm.tsx
@@ -0,0 +1,57 @@
+// LoginForm.tsx
+import React, { useState } from 'react';
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your login logic here
+    console.log('Login clicked with:', { email, password });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-semibold mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-600 text-sm font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 p-2 w-full border rounded"
+              placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-600 text-sm font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 p-2 w-full border rounded"
+              placeholder="Your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="button"
+            className="w-full bg-blue-500 text-white p-2 rounded"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
